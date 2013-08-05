46ceafb80bd1ee16482a5b98ee9e624723b7bf19
cammoraton <ncammorato@wst100166.terc.edu>
2013-08-05 14:22:49 -0400

    Split huge method up a bit.  Cache parsing is now done instead of working against previous query.

--- lib/vex/dsl/configuration/cache.rb
+++ lib/vex/dsl/configuration/cache.rb
@@ -26,18 +26,11 @@ module Vex
         end
         
         private
-        # This is one hell of a method.  Lots of "code smell"
-        def update_cache
-          logger.info("[INFO]  - update_cache: Updating cache for object id: #{self._id}")
-          old_cache = self.cache.dup  # Duplicate cache
-          logger.debug("[DEBUG] - update_cache: old cache is #{self.cache.to_json}")
-          self.cache = Array.new      # Clear cache
-          
-          # Set up our query results container
-          assignments = Hash.new
-          query_depth = 0
-          # Need to pass this through the class the module is included in or else it will wind up in the module namespace
+        def cache_queries
+          queries = Hash.new
+          # Need to pass this through the class we're mixed in with or else it will default to the module
           query_interface = self.class.const_get("Configuration")
+          query_depth = 0
           # Figure out how many levels of nesting we need to satisfy dependencies and assignments
           self.vex_assignments.keys.each do |key|
             unless self.vex_assignments[key].nil? or self.vex_assignments[key][:through].nil?
@@ -55,107 +48,105 @@ module Vex
           logger.debug("[DEBUG] - update_cache: Calculated query depth of #{query_depth}")
           
           # Now construct and execute the queries
-          assignments[0]  = query_interface.where(:_id => { :$in => self.assignment_ids }).fields(:name, :_id, :_type, :assignment_ids, :cache).all
-          logger.debug("[DEBUG] - update_cache: Executed query for depth 0, found #{assignments[0].count} records")
+          queries[0]  = query_interface.where(:_id => { :$in => self.assignment_ids }).fields(:name, :_id, :_type, :assignment_ids, :cache).all
+          logger.debug("[DEBUG] - update_cache: Executed query for depth 0, found #{queries[0].count} records")
           # First level of inheritance is special
           unless query_depth < 1
-            assignments[1] = query_interface.where({ :$and => [{ :assignment_ids => {:$in => self.assignment_ids}},
-                                                             { :_id => { :$nin => self.assignment_ids + [ self._id ] }}]}).fields(:name, :_id, :_type, :assignment_ids, :cache).all
-            logger.debug("[DEBUG] - update_cache: Executed query for depth 1, found #{assignments[1].count} records")
+            queries[1] = query_interface.where({ :$and => [{ :assignment_ids => {:$in => self.assignment_ids}},
+                                                           { :_id => { :$nin => self.assignment_ids + [ self._id ] }}]}).fields(:name, :_id, :_type, :assignment_ids, :cache).all
+            logger.debug("[DEBUG] - update_cache: Executed query for depth 1, found #{queries[1].count} records")
             # Now go through through the rest
             unless query_depth < 2
               (2..query_depth).each do |depth|
                 current_ids = [ self._id ] 
                 (0..depth-1).each do |current|
-                  current_ids = (current_ids + assignments[current].map{|a| a._id}).uniq
+                  current_ids = (current_ids + queries[current].map{|a| a._id}).uniq
                 end
                 assign_ids = Array.new
-                assignments[depth-1].each do |assign|
+                queries[depth-1].each do |assign|
                   assign_ids = (assign_ids + assign.assignment_ids).uniq
                 end
-                assignments[depth] = query_interface.where({ :$and => [{:assignment_ids => {:$in => assign_ids}},
-                                                                     {:_id => {:$nin => current_ids }}]}).fields(:name, :_id, :_type, :assignment_ids, :cache).all
-                logger.debug("[DEBUG] - update_cache: Executed query for depth #{depth}, found #{assignments[depth].count} records")
+                queries[depth] = query_interface.where({ :$and => [{:assignment_ids => {:$in => assign_ids}},
+                                                                   {:_id => {:$nin => current_ids }}]}).fields(:name, :_id, :_type, :assignment_ids, :cache).all
+                logger.debug("[DEBUG] - update_cache: Executed query for depth #{depth}, found #{queries[depth].count} records")
               end
             end
           end 
+          return queries
+        end
+        
+        def parse_cache_query_results(query, check)
+          ids = Array.new
+          check.each do |build|
+            ids = (ids + [ build["id"] ]).uniq
+          end
+          query.each do |evaluate|
+            inherited_ids = Array.new
+            check.select{ |a| a if evaluate.assignment_ids.include?(BSON::ObjectId.from_string(a["id"])) }.each do |inherited|
+              inherited_ids = (inherited_ids + [ inherited["id"] ]).uniq
+            end
+            
+            logger.debug( "Inherited #{evaluate._id} from #{inherited_ids.to_json}")
+            next if inherited_ids.empty?
+            
+            cache_check = self.cache.select{ |a| a if a["id"].to_s == evaluate._id.to_s }  
+            if cache_check.empty?
+              inherited_ids = inherited_ids.first unless inherited_ids.count > 1
+              self.cache.push({ "id" => evaluate._id.to_s, 
+                                "type" => evaluate._type.to_s, 
+                                "name" => evaluate.name, 
+                                "source" => inherited_ids, 
+                                "dependency_only" => false })
+            else
+              cache_check.each do |update|
+                if update["source"].is_a? Array
+                  inherited_ids = ( inherited_ids + update["source"]).uniq
+                else
+                  inherited_ids = ( inherited_ids + [ update["source"] ]).uniq
+                end
+                inherited_ids = inherited_ids.first unless inherited_ids.count > 1
+                update["source"] = inherited_ids
+              end
+            end
+          end
+        end
+        
+        def update_cache
+          logger.info("[INFO]  - update_cache: Updating cache for object id: #{self._id}")
+          old_cache = self.cache.dup  # Duplicate cache
+          logger.debug("[DEBUG] - update_cache: old cache is #{self.cache.to_json}")
+          self.cache = Array.new      # Clear cache
+          
+          assignments = cache_queries
+          query_depth = assignments.keys.count - 1
           
           logger.debug("[DEBUG] - update_cache: Updating cache for direct assignments")
           self.vex_assignments.keys.each do |key|
             klass = self.class.const_get(key.to_s.singularize.camelize)
-            logger.debug("[DEBUG] - update_cache: Updating cache for direct assignments, processing class #{key.to_s.singularize.camelize}, #{assignments[0].select{ |a| a if a._type.to_s == klass.to_s}.count} records")
             assignments[0].select{ |a| a if a._type.to_s == klass.to_s}.each do |eval|
-              self.cache.push({ "id" => eval._id.to_s, "type" => eval._type.to_s, "name" => eval.name, "source" => self._id.to_s, "dependency_only" => false })
+              self.cache.push({ "id" => eval._id.to_s, 
+                                "type" => eval._type.to_s, 
+                                "name" => eval.name, 
+                                "source" => self._id.to_s, 
+                                "dependency_only" => false })
             end
           end
           
           unless query_depth < 1
             logger.debug("[DEBUG] - update_cache: Updating cache for depth level 1 assignments")
             self.vex_assignments.keys.each do |key|
-              klass = self.class.const_get(key.to_s.singularize.camelize)
-              parse = self.cache.select{ |a| a if a.type.to_s == klass.to_s }
-              logger.debug("[DEBUG] - update_cache: Updating cache for depth level 1 assignments, processing class #{key.to_s.singularize.camelize}, #{assignments[1].select{ |a| a if a._type.to_s == klass.to_s}.count} possible records")           
+              klass = self.class.const_get(key.to_s.singularize.camelize) 
               eval = self.vex_assignments[key][:through]
               if eval.is_a? Array
                 eval.each do |evaluate|
                   eval_klass = self.class.const_get(evaluate.to_s.singularize.camelize)
-                  eval_items = assignments[0].select{|a| a if a.type.to_s == eval_klass.to_s }
-                  ids = Array.new
-                  eval_items.each do |iterate|
-                    ids = (ids + iterate.assignment_ids).uniq
-                  end
-                  check = assignments[1].select{ |a| a if a.type.to_s == klass.to_s and ids.include?(a._id) }
-                  check.each do |push|
-                    inherited_ids = Array.new
-                    eval_items.select{ |a| a if a.assignment_ids.include?(push._id) }.each do |inherited|
-                      inherited_ids = (inherited_ids + [inherited._id.to_s]).uniq
-                    end
-                    cache_check = self.cache.select{ |a| a if a["id"].to_s == push._id.to_s }
-                    if cache_check.empty?
-                      inherited_ids = inherited_ids.first unless inherited_ids.count > 1
-                      self.cache.push({ "id" => push._id.to_s, "type" => push._type.to_s, "name" => push.name, "source" => inherited_ids, "dependency_only" => false })
-                    else
-                      cache_check.each do |update|
-                        if update["source"].is_a? Array
-                          inherited_ids = ( inherited_ids + update["source"]).uniq
-                        else
-                          inherited_ids = ( inherited_ids + [ update["source"] ]).uniq
-                        end
-                        inherited_ids = inherited_ids.first unless inherited_ids.count > 1
-                        update["source"] = inherited_ids
-                      end
-                    end
-                  end
+                  parse_cache_query_results(assignments[1].select{ |a| a if a.type.to_s == klass.to_s }, 
+                                            self.cache.select{ |a| a if a["type"] == eval_klass.to_s})
                 end
               elsif !eval.nil?
                 eval_klass = self.class.const_get(eval.to_s.singularize.camelize)
-                eval_items = assignments[0].select{|a| a if a.type.to_s == eval_klass.to_s }
-                ids = Array.new
-                eval_items.each do |iterate|
-                  ids = (ids + iterate.assignment_ids).uniq
-                end
-                check = assignments[1].select{ |a| a if a.type.to_s == klass.to_s and ids.include?(a._id) }
-                check.each do |push|
-                  inherited_ids = Array.new
-                  eval_items.select{ |a| a if a.assignment_ids.include?(push._id) }.each do |inherited|
-                    inherited_ids = (inherited_ids + [inherited._id.to_s]).uniq
-                  end
-                  cache_check = self.cache.select{ |a| a if a["id"].to_s == push._id.to_s }
-                  if cache_check.empty?
-                    inherited_ids = inherited_ids.first unless inherited_ids.count > 1
-                    self.cache.push({ "id" => push._id.to_s, "type" => push._type.to_s, "name" => push.name, "source" => inherited_ids, "dependency_only" => false })
-                  else
-                    cache_check.each do |update|
-                      if update["source"].is_a? Array
-                        inherited_ids = ( inherited_ids + update["source"]).uniq
-                      else
-                        inherited_ids = ( inherited_ids + [ update["source"] ]).uniq
-                      end
-                      inherited_ids = inherited_ids.first unless inherited_ids.count > 1
-                      update["source"] = inherited_ids
-                    end
-                  end
-                end
+                parse_cache_query_results(assignments[1].select{ |a| a if a.type.to_s == klass.to_s }, 
+                                          self.cache.select{ |a| a if a["type"] == eval_klass.to_s} )
               end
             end
             unless query_depth < 2
