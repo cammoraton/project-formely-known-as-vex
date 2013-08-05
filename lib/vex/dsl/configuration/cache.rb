f6932fc57deba007ffe5329ef38b298e76ec9ca1
cammoraton <ncammorato@wst100166.terc.edu>
2013-08-05 11:08:46 -0400

    Fixed up longer depth queries.

--- lib/vex/dsl/configuration/cache.rb
+++ lib/vex/dsl/configuration/cache.rb
@@ -28,13 +28,16 @@ module Vex
         private
         # This is one hell of a method.
         def update_cache
-          logger.debug("[DEBUG] - update_cache: Updating cache for object id: #{self._id}")
+          logger.info("[INFO]  - update_cache: Updating cache for object id: #{self._id}")
           old_cache = self.cache.dup  # Duplicate cache
+          logger.debug("[DEBUG] - update_cache: old cache is #{self.cache.to_json}")
           self.cache = Array.new      # Clear cache
           
           # Set up our query results container
           assignments = Hash.new
           query_depth = 0
+          # Need to pass this through the class the module is included in or else it will wind up in the module namespace
+          query_interface = self.class.const_get("Configuration")
           # Figure out how many levels of nesting we need to satisfy dependencies and assignments
           self.vex_assignments.keys.each do |key|
             unless self.vex_assignments[key].nil? or self.vex_assignments[key][:through].nil?
@@ -52,22 +55,26 @@ module Vex
           logger.debug("[DEBUG] - update_cache: Calculated query depth of #{query_depth}")
           
           # Now construct and execute the queries
-          assignments[0]  = Configuration.where(:_id => { :$in => self.assignment_ids }).fields(:name, :_id, :_type, :assignment_ids).all
+          assignments[0]  = query_interface.where(:_id => { :$in => self.assignment_ids }).fields(:name, :_id, :_type, :assignment_ids, :cache).all
           logger.debug("[DEBUG] - update_cache: Executed query for depth 0, found #{assignments[0].count} records")
           # First level of inheritance is special
           unless query_depth < 1
-            assignments[1] = Configuration.where({ :$and => [{ :assignment_ids => {:$in => self.assignment_ids}},
-                                                             { :_id => { :$nin => self.assignment_ids }}]}).fields(:name, :_id, :_type, :assignment_ids).all
+            assignments[1] = query_interface.where({ :$and => [{ :assignment_ids => {:$in => self.assignment_ids}},
+                                                             { :_id => { :$nin => self.assignment_ids }}]}).fields(:name, :_id, :_type, :assignment_ids, :cache).all
             logger.debug("[DEBUG] - update_cache: Executed query for depth 1, found #{assignments[1].count} records")
             # Now go through through the rest
             unless query_depth < 2
-            (2..query_depth).each do |depth|
+              (2..query_depth).each do |depth|
                 current_ids = Array.new
                 (0..depth-1).each do |current|
                   current_ids = (current_ids + assignments[current].map{|a| a._id}).uniq
                 end
-                assignments[depth] = Configuration.where({ :$and => [{:assignment_ids => {:$in => assignments[depth-1].map{|a| a.assignment_ids }}},
-                                                                     {:_id => {:$nin => current_ids }}]}).fields(:name, :_id, :_type, :assignment_ids).all
+                assign_ids = Array.new
+                assignments[depth-1].each do |assign|
+                  assign_ids = (assign_ids + assign.assignment_ids).uniq
+                end
+                assignments[depth] = query_interface.where({ :$and => [{:assignment_ids => {:$in => assign_ids}},
+                                                                     {:_id => {:$nin => current_ids }}]}).fields(:name, :_id, :_type, :assignment_ids, :cache).all
                 logger.debug("[DEBUG] - update_cache: Executed query for depth #{depth}, found #{assignments[depth].count} records")
               end
             end
@@ -76,9 +83,29 @@ module Vex
           logger.debug("[DEBUG] - update_cache: Updating cache for direct assignments")
           self.vex_assignments.keys.each do |key|
             klass = self.class.const_get(key.to_s.singularize.camelize)
-            logger.debug("[DEBUG] - update_cache: Updating cache for direct assignments, processing class #{key.to_s.singularize.camelize}, #{assignments[0].select{ |a| a if a._type == klass}.count} records")
-            assignments[0].select{ |a| a if a._type == klass}.each do |eval|
+            logger.debug("[DEBUG] - update_cache: Updating cache for direct assignments, processing class #{key.to_s.singularize.camelize}, #{assignments[0].select{ |a| a if a._type.to_s == klass.to_s}.count} records")
+            assignments[0].select{ |a| a if a._type.to_s == klass.to_s}.each do |eval|
               self.cache.push({ "id" => eval._id.to_s, "type" => eval._type.to_s, "name" => eval.name, "source" => self._id.to_s, "dependency_only" => false })
+            end
+          end
+          
+          unless query_depth < 1
+            logger.debug("[DEBUG] - update_cache: Updating cache for depth level 1 assignments")
+            self.vex_assignments.keys.each do |key|
+              klass = self.class.const_get(key.to_s.singularize.camelize)
+              parse = self.cache.select{ |a| a if a.type.to_s == klass.to_s }
+              eval = self.vex_assignments[key][:through]
+              if eval.is_a? Array
+                eval.each do |evaluate|
+                  check = assignments[1].select{ |a| a if a.type.to_s == klass.to_s }
+                end
+              else
+                
+              end
+            end
+            unless query_depth < 2
+              (2..query_depth).each do |depth|
+              end
             end
           end
             
