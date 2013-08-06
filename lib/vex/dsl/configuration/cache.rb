e77f5eae0431e2caa1ce60e1db295a0ee4b3d7b5
cammoraton <ncammorato@osxp-ncammorato.home>
2013-08-06 19:56:08 -0400

    Made cache cascade save more selective.

--- lib/vex/dsl/configuration/cache.rb
+++ lib/vex/dsl/configuration/cache.rb
@@ -26,6 +26,18 @@ module Vex
           @cascade_pending ||= []
         end
         
+        def cascade_pending=(val)
+          @cascade_pending = val
+        end
+        
+        def cascaded
+          @cascaded ||= []
+        end
+        
+        def cascaded=(val)
+          @cascaded = val
+        end
+        
         def cache?
           self.class.vex_cached
         end
@@ -39,21 +51,25 @@ module Vex
           self.cache = self.vex_associations.reload # Force a requery
           logger.debug("[DEBUG] - update_cache: cache updated, is now #{self.cache.to_json}")  
           
-          cascade_save    = self.cache.select{|a| a["id"] if (a["dependency_only"].nil? or a["dependency_only"] == false)} - old_cache.select{|a| a["id"] if (a["dependency_only"].nil? or a["dependency_only"] == false)}
-          cascade_deleted = old_cache.select{|a| a["id"] if (a["dependency_only"].nil? or a["dependency_only"] == false)} - self.cache.select{|a| a["id"] if (a["dependency_only"].nil? or a["dependency_only"] == false)}
+          cascade_save    = self.cache.select{|a| a if (a["dependency_only"].nil? or a["dependency_only"] == false)}.map{|a| a["id"]} - 
+                             old_cache.select{|a| a if (a["dependency_only"].nil? or a["dependency_only"] == false)}.map{|a| a["id"]}
+          cascade_deleted =  old_cache.select{|a| a if (a["dependency_only"].nil? or a["dependency_only"] == false)}.map{|a| a["id"]} - 
+                            self.cache.select{|a| a if (a["dependency_only"].nil? or a["dependency_only"] == false)}.map{|a| a["id"]}
           # Combine and remove any direct associations( fixup_assignments will take care of those)
-          @cascade_pending = ((cascade_save + cascade_deleted) - 
-                             self.vex_associations.to_a.select{|a| a["id"] if a["source"].to_s == self._id.to_s}).uniq
+          self.cascade_pending = ((cascade_save + cascade_deleted) - 
+                                  self.vex_associations.to_a.select{|a| a if a["source"].to_s == self._id.to_s}.map{|a| a["id"]}).uniq
           logger.debug("[DEBUG] - update_cache: new or deleted ids #{@cascade.to_json}")  
         end
         
         def cascade_save
-          ids = self.cascade_pending.map{|a| BSON::ObjectId(a)}
+          ids = (self.cascade_pending - self.cascaded).uniq.map{|a| BSON::ObjectId(a)}
           query_wrapper = self.class.const_get("Configuration")
           query_wrapper.where(:_id => {:$in => ids }).all.each do |cascade|
+            cascade.cascaded = (self.cascade_pending + self.cascaded).uniq
             cascade.touch; cascade.save; cascade.reload
           end
-          @cascade_pending = []
+          self.cascade_pending = []
+          self.cascaded = []
         end
       end
     end
