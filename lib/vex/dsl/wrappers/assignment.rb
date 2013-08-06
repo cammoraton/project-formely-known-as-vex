46ddc7fe9d25899c4a4aa233354d98ffa1899c9e
cammoraton <ncammorato@osxp-ncammorato.home>
2013-08-05 21:10:55 -0400

    Association and assignment wrappers

---
+++ lib/vex/dsl/wrappers/assignment.rb
@@ -0,0 +1,63 @@
+module Vex
+  module Dsl
+    module Wrappers
+      module Assignment
+        class Wrapper
+          def initialize(object, klass)
+            @object = object
+            @klass  = klass
+            @associations = @object.vex_associations.by_type(klass.to_s)
+          end
+          
+          # We're really an array.  We swear!
+          def method_missing(m, *args, &block)
+            self.to_a.send m, *args, &block
+          end
+          
+          # Refresh from associations
+          def refresh
+            @associations = @object.vex_associations.by_type(klass.to_s).dup
+          end
+          
+          def to_a
+            @associations #||= @object.vex_associations.by_type(klass.to_s) # This caused stack level too deep errors chaining off method_missing.
+                                                                            # Initialize it in the initializer and all is golden.  Something about ruby I
+                                                                            # don't understand is going on here.
+          end
+          
+          def local
+            self.to_a.select{|a| a if a["source"].to_s == @object._id.to_s }
+          end
+          
+          def source(id)
+            inherited_object = self.to_a.select{|a| a if a["id"] == id }   # Find the damn thing!
+            return nil if inherited_object.nil? or inherited_object.empty? # This should never happen unless we get a bad id
+            inherited_object = inherited_object.first                      # There should only be one, but....
+            source = @object.vex_associations.to_a.select{|a| a if a["id"] == inherited_object["source"]}
+            return nil if source.nil? or source.empty?                     # This should never happen
+            return source.map{ |a| { :type => a["type"], :name => a["name"], :id => a["id"]}}
+          end
+          
+          def ids=(val)
+            raise(ArgumentError, "Token ids must be passed as an array") unless val.is_a? Array
+            current_ids = self.local.map{ |a| a["id"].to_s}
+            (val - current_ids).each do |add|
+              @object.assignment_ids << BSON::ObjectId(add)
+            end
+            (current_ids - val).each do |del|
+              @object.assignment_ids.delete(BSON::ObjectId(del))
+            end
+            # TODO: update cache? Do we ever do this without saving?
+          end
+          
+          # Shovel assignment
+          def <<(val)
+            raise(ArgumentError, "Can only add values of type #{@klass.to_s}") unless val.is_a? @klass
+            @object.assignment_ids.push(val._id) unless @object.assignment_ids.include?(val._id)
+            # TODO: push to cache?  Do we ever do this without saving?
+          end
+        end
+      end
+    end
+  end
+end
--- lib/vex/dsl/wrappers/association.rb
+++ lib/vex/dsl/wrappers/association.rb
@@ -2,80 +2,183 @@ module Vex
   module Dsl
     module Wrappers
       module Association
+        # Constructs queries and filters them against Assignments, or passes through to cache
         class Wrapper
-          def initialize(object, klass, options)
-            @parent = object
-            @klass = klass
-            @options = options
+          def initialize(object)
+            @object = object
           end
           
           # We're really an array, we swear
           def method_missing(m, *args, &block) 
             self.to_a.send m, *args, &block
           end
-             
-          def reload
-            @local_only = @klass.where( :_id => { :$in => @parent.assignment_ids } ).all
-            @assignments = query
-          end
           
           def to_a
-            @assignments ||= query
-          end
-    
-          def local
-            @local_only ||= @klass.where( :_id => { :$in => @parent.assignment_ids } ).all
-          end
-    
-          def find_by_id(id)
-            self.to_a.each do |assign|
-              return assign if assign._id.to_s == id.to_s
-            end
-            return nil
-          end
-    
-          # For tokeninput assignment
-          def ids=(val)
-            raise(ArgumentError, "ids must be passed as an array") unless val.is_a? Array
-            current_ids = self.local.map{ |a| a._id.to_s}
-            (val - current_ids).each do |add|
-              @parent.assignment_ids << BSON::ObjectId(add)
-            end
-            (current_ids - val).each do |del|
-              @parent.assignment_ids.delete(BSON::ObjectId(del))
+            unless @object.has_cache?
+              @associations ||= run_queries
+            else
+              @object.cache
             end
           end
+                
+          def reload
+            # When we call reload we always execute our queries
+            @query_depth  = calculate_query_depth
+            @queries      = construct_queries
+            @associations = run_queries
+            return @associations
+          end 
+          
+          def query_depth
+            @query_depth ||= calculate_query_depth
+          end
+                    
+          def queries
+            @queries ||= construct_queries
+          end
     
-          # Shovel assignment
-          def <<(val)
-            raise(ArgumentError, "Can only add values of type #{@klass.to_s}") unless val.is_a? @klass
-            @object.assignment_ids.push(val._id) unless @object.assignment_ids.include?(val._id)
-            self.to_a.push(val) unless self.to_a.include?(val)
+          # Sugar methods
+          def by_type(type)
+            self.to_a.select{ |a| a if a["type"].to_s == type.to_s }
           end
           
           private
-          def query
-            results = self.local
-            unless @options[:through].nil?
-              if @options[:through].is_a? Array
-                @options[:through].each do |inherit|
-                  eval = @parent.send(inherit).to_a
-                  unless eval.empty?
-                    eval.each do |item|
-                      results += item.send(@klass.to_s.downcase.pluralize).to_a
-                    end
-                  end
-                end
-              else
-                eval = @parent.send(@options[:through]).to_a
-                unless eval.empty?
-                  eval.each do |item|
-                    results += item.send(@klass.to_s.downcase.pluralize).to_a
+          def calculate_query_depth
+            query_depth = 0
+            
+            @object.vex_assignments.keys.select{ |a| a unless @object.vex_assignments[a].nil? or @object.vex_assignments[a][:through].nil? }.each do |key|
+              query_depth = 1 if query_depth < 1
+              eval = @object.vex_assignments[key][:through]
+              if eval.is_a? Array
+                eval.each do |through|
+                  unless @object.vex_assignments[through].nil? or @object.vex_assignments[through][:through].nil?
+                    query_depth = 2 if query_depth < 2
                   end
                 end
               end
             end
-            results.uniq ||= []
+            return query_depth
+          end
+          
+          def construct_queries
+            queries = Hash.new
+            # Need to pass this through the class we're mixed in with or else it will default to the module
+            query_interface = @object.class.const_get("Configuration")
+            
+            # Now construct and execute the queries
+            queries[0]  = query_interface.where(:_id => { :$in => @object.assignment_ids }).fields(:name, :_id, :_type, :assignment_ids).all
+            # First level of inheritance is special
+            unless self.query_depth < 1
+              queries[1] = query_interface.where({ :$and => [{ :assignment_ids => {:$in => @object.assignment_ids}},
+                                                           { :_id => { :$nin => @object.assignment_ids + [ @object._id ] }}]}).fields(:name, :_id, :_type, :assignment_ids).all
+              # Now go through through the rest
+              unless self.query_depth < 2
+                (2..self.query_depth).each do |depth|
+                  current_ids = [ @object._id ] 
+                  (0..depth-1).each do |current|
+                    current_ids = (current_ids + queries[current].map{|a| a._id}).uniq
+                  end
+                  assign_ids = Array.new
+                  queries[depth-1].each do |assign|
+                    assign_ids = (assign_ids + assign.assignment_ids).uniq
+                  end
+                  queries[depth] = query_interface.where({ :$and => [{:assignment_ids => {:$in => assign_ids}},
+                                                                   {:_id => {:$nin => current_ids }}]}).fields(:name, :_id, :_type, :assignment_ids).all
+                end
+              end
+            end 
+            return queries
+          end
+          
+          def parse_query(query, check)
+            ids = Array.new
+            check.each do |build|
+              ids = (ids + [ build["id"] ]).uniq
+            end
+            query.each do |evaluate|
+              inherited_ids = Array.new
+              check.select{ |a| a if evaluate.assignment_ids.include?(BSON::ObjectId.from_string(a["id"])) }.each do |inherited|
+                inherited_ids = (inherited_ids + [ inherited["id"] ]).uniq
+              end
+            
+              next if inherited_ids.empty?
+            
+              cache_check = @associations.select{ |a| a if a["id"].to_s == evaluate._id.to_s }  
+              if cache_check.empty?
+                inherited_ids = inherited_ids.first unless inherited_ids.count > 1
+                @associations.push({ "id"     => evaluate._id.to_s, 
+                                     "type"   => evaluate._type.to_s, 
+                                     "name"   => evaluate.name, 
+                                     "source" => inherited_ids })
+              else
+                cache_check.each do |update|
+                  if update["source"].is_a? Array
+                    inherited_ids = ( inherited_ids + update["source"]).uniq
+                  else
+                    inherited_ids = ( inherited_ids + [ update["source"] ]).uniq
+                  end
+                  inherited_ids = inherited_ids.first unless inherited_ids.count > 1
+                  update["source"] = inherited_ids
+                end
+              end
+            end
+          end
+          
+          def run_queries
+            @associations = Array.new
+          
+            assignments = self.queries
+            query_depth = assignments.keys.count - 1
+          
+            @object.vex_assignments.keys.each do |key|
+              klass = @object.class.const_get(key.to_s.singularize.camelize)
+              assignments[0].select{ |a| a if a._type.to_s == klass.to_s}.each do |eval|
+                @associations.push({ "id"     => eval._id.to_s, 
+                                     "type"   => eval._type.to_s, 
+                                     "name"   => eval.name, 
+                                     "source" => @object._id.to_s })
+              end
+            end
+          
+            unless query_depth < 1
+              @object.vex_assignments.keys.select{ |a| a unless @object.vex_assignments[a][:through].nil? }.each do |key|
+                klass = @object.class.const_get(key.to_s.singularize.camelize) 
+                eval = @object.vex_assignments[key][:through]
+                if eval.is_a? Array
+                  eval.each do |evaluate|
+                    eval_klass = @object.class.const_get(evaluate.to_s.singularize.camelize)
+                    parse_query(assignments[1].select{ |a| a if a.type.to_s == klass.to_s }, 
+                                @associations.select{ |a| a if a["type"] == eval_klass.to_s})
+                  end
+                else
+                  eval_klass = @object.class.const_get(eval.to_s.singularize.camelize)
+                  parse_query(assignments[1].select{ |a| a if a.type.to_s == klass.to_s }, 
+                              @associations.select{ |a| a if a["type"] == eval_klass.to_s} )
+                end
+              end
+              unless query_depth < 2
+                # We need to iterate through our relationships
+                (2..query_depth).each do |depth|
+                  @object.vex_assignments.keys.select{ |a| a unless @object.vex_assignments[a][:through].nil? }.each do |key|
+                    klass = @object.class.const_get(key.to_s.singularize.camelize) 
+                    eval = @object.vex_assignments[key][:through] 
+                    if eval.is_a? Array
+                      eval.each do |evaluate|
+                        eval_klass = @object.class.const_get(evaluate.to_s.singularize.camelize)
+                        parse_query(assignments[depth].select{ |a| a if a.type.to_s == klass.to_s }, 
+                                    @associations.select{ |a| a if a["type"] == eval_klass.to_s})
+                      end
+                    else
+                      eval_klass = @object.class.const_get(eval.to_s.singularize.camelize)
+                      parse_query(assignments[depth].select{ |a| a if a.type.to_s == klass.to_s }, 
+                                  @associations.select{ |a| a if a["type"] == eval_klass.to_s} )
+                    
+                    end
+                  end
+                end
+              end
+            end
+            return @associations
           end
         end
       end
