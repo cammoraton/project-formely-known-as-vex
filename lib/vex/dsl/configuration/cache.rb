233a937b464e2d3aaab636131ab49b48c8894afb
cammoraton <ncammorato@osxp-ncammorato.home>
2013-08-05 21:48:53 -0400

    Cascade!

--- lib/vex/dsl/configuration/cache.rb
+++ lib/vex/dsl/configuration/cache.rb
@@ -14,11 +14,16 @@ module Vex
             key         :cache, Array
             
             before_save :update_cache
+            after_save  :cascade_save
           end
         end
         
         def self.included(base)
           base.extend ClassMethods
+        end
+        
+        def cascade
+          @cascade ||= []
         end
         
         def has_cache?
@@ -34,8 +39,21 @@ module Vex
           self.cache = self.vex_associations.reload # Force a requery
           logger.debug("[DEBUG] - update_cache: cache updated, is now #{self.cache.to_json}")  
           
-          cascade = self.cache.map{|a| a["id"]} - old_cache.map{|a| a["id"]}
-          logger.debug("[DEBUG] - update_cache: new items #{cascade.to_json}")  
+          cascade_save    = self.cache.map{|a| a["id"]} - old_cache.map{|a| a["id"]}
+          cascade_deleted = old_cache.map{|a| a["id"]} - self.cache.map{|a| a["id"]}
+          # Combine and remove any direct associations( fixup_assignments will take care of those)
+          @cascade = ((cascade_save + cascade_deleted) - 
+                    self.vex_associations.to_a.select{|a| a["id"] if a["source"].to_s == self._id.to_s}).uniq
+          logger.debug("[DEBUG] - update_cache: new or deleted ids #{@cascade.to_json}")  
+        end
+        
+        def cascade_save
+          ids = self.cascade.map{|a| BSON::ObjectId(a)}
+          query_wrapper = self.class.const_get("Configuration")
+          query_wrapper.where(:_id => {:$in => ids }).all.each do |cascade|
+            cascade.touch; cascade.save; cascade.reload
+          end
+          @cascade = []
         end
       end
     end
