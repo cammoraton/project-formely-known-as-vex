a0bb3e7f54dc2cc231387641908f343bcd9e22d8
cammoraton <nick_cammorato@terc.edu>
2013-08-13 12:37:12 -0400

    Cleared embedded configs for now, reset configuration_spec for test creation.

--- app/models/embedded_configuration.rb
+++
@@ -1,6 +0,0 @@
-class EmbeddedConfiguration
-  include MongoMapper::EmbeddedDocument
-
-  key :name, String, :required => true
-  key :data, Hash
-end
--- spec/models/configuration_spec.rb
+++ spec/models/configuration_spec.rb
@@ -1,21 +1,5 @@
 require 'spec_helper'
 
-shared_examples_for "configuration" do
-  #it { testobject.should validate_uniqueness_of(:name) }
-  #it { testobject.should validate_presence_of(:name) }
-end
-
-Vex::Application.config.dynamic_models.each do |item|
-  klass = self.class.const_get(item.singularize.camelize)
-  describe klass do
-    let(:testobject) { klass.new }
-    include_examples "configuration"
-    klass.vex_assignments.keys.each do |association|
-      describe association do
-        #it "should default to an empty array" do
-        #  testobject.send(association).should be_empty
-        #end
-      end
-    end
-  end 
+describe Configuration do
+  
 end
