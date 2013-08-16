631d85ce4e52a1bc4e04403991e0211babf64880
cammoraton <nick_cammorato@terc.edu>
2013-08-16 08:41:57 -0400

    Updated cache tests.

--- spec/lib/vex/configuration/cache_spec.rb
+++ spec/lib/vex/configuration/cache_spec.rb
@@ -22,9 +22,7 @@ describe Vex::Dsl::Configuration::Cache do
       end 
     end
      
-    class CacheTestNone < Configuration
-       
-    end
+    class CacheTestNone < Configuration; end;
   end
   
   describe "#ClassMethods" do
@@ -44,5 +42,23 @@ describe Vex::Dsl::Configuration::Cache do
       CacheTestNone.new.has_cache?().should be_false
       CacheTestA.new.has_cache?().should be_true
     end
+  end
+  
+  describe "#cache" do
+    it "should exist as a method if has_cache has been called" do
+      CacheTestA.new.respond_to?(:cache).should be_true
+    end
+    
+    it "should not exist if has_cache has not been called" do
+      CacheTestNone.new.respond_to?(:cache).should be_false
+    end
+    
+    it "should be an array" do
+      CacheTestA.new.cache.is_a?(Array).should be_true
+    end
+  end
+  
+  describe "#update_cache" do
+    it "should have tests"
   end
 end
