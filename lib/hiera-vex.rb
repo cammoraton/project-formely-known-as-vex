a4a35026b4c87c8d7d7fe25a01eb4a826e96ed49
cammoraton <nick_cammorato@terc.edu>
2013-08-15 20:35:07 -0400

    Hiera skeleton.

---
+++ lib/hiera-vex.rb
@@ -0,0 +1,21 @@
+class Hiera
+  module Backend
+    class Module_vex_backend
+      def initialize
+        Hiera.debug("Hiera Module Vex backend starting")
+      end
+      
+      def load_module_config(module_name, environment)
+        
+        
+      end
+      def load_data(path)
+        
+      end
+      
+      def lookup(key, scope, order_override, resolution_type)
+        
+      end
+    end
+  end
+end
