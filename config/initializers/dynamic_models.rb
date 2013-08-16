21bbde5145ec7d23cd321994d88ddf95532016d3
cammoraton <nick_cammorato@terc.edu>
2013-08-16 15:43:25 -0400

    Removed puts statement.  Not required.

--- config/initializers/dynamic_models.rb
+++ config/initializers/dynamic_models.rb
@@ -3,5 +3,4 @@ require 'vex'
 
 parser = Vex::Dsl::Parser.new(YAML::load(File.open("#{Rails.root}/config/models.yml", "r")))
 Vex::Application.config.dynamic_models = parser.classes.map{ |a| a.to_s.singularize }
-puts Vex::Application.config.dynamic_models.inspect
 parser.load_all(Configuration)
