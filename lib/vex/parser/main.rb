88b180a62e3d2efe3f50ad317ba837dd35cf1de3
cammoraton <nick_cammorato@terc.edu>
2013-08-21 08:17:43 -0400

    Add in database and base_object keywords.

--- lib/vex/parser/main.rb
+++ lib/vex/parser/main.rb
@@ -5,6 +5,14 @@ module Vex
         line_parser(lines)
       end
       
+      def base_object(arg)
+        Vex::Application.config.dashboard_base_object = arg
+      end
+      
+      def database(args)
+        puts "Database: #{args.inspect}"
+      end
+      
       def cache(arg)
         case arg.downcase
         when "false"
